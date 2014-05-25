805dba7c2bf1d6d0bf894246e6f9706ff0a15649
Jesse McGinnis <jessieelsom@hotmail.com>
2014-05-25 11:22:22 -0400

    Form tweaks

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :update_sanitized_params, if: :devise_controller?
 
-  def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:given_name, :family_name, :email, :gender, :street_address, :city, :postal_code, :country_name, :password, :password_confirmation)
-    end
+protected
 
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:given_name, :family_name, :email, :gender, :street_address, :city, :postal_code, :country_name, :password, :password_confirmation, :current_password)
-    end
+  def update_sanitized_params
+    standard_params = %i(given_name family_name email gender street_address city postal_code country_name
+                         password password_confirmation)
+    devise_parameter_sanitizer.for(:sign_up) << standard_params
+    devise_parameter_sanitizer.for(:account_update) << (standard_params << :current_password)
   end
 end
